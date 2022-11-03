bd80bdeb5e9063556b0817110b481bbb24d29dbd
Alexandre Del Vecchio <alexandre@buyco.co>
2022-11-03 11:33:52 +0100

    Feat(structure): Try to fix goroutine leak on consumption burst
    * Move Go to 1.19
    * Move from deprecated golint to staticcheck ASAP

--- internal/filesystem/filesystem.go
+++ internal/filesystem/filesystem.go
@@ -1,6 +1,7 @@
 package filesystem
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	log "github.com/sirupsen/logrus"
@@ -10,12 +11,9 @@ import (
 	"rocket-storagemanager/internal/kafka"
 	"rocket-storagemanager/internal/model/config"
 	"rocket-storagemanager/internal/model/events"
+	"sync"
 	"time"
 )
-
-var logDataDog *log.Logger
-
-type ErrorType string
 
 const (
 	NoError       ErrorType = "NoError"
@@ -25,27 +23,43 @@ const (
 	GeneralError            = "GeneralError"
 )
 
-func findDirectories(workflowId string, relativeWorkspacePath string) []string {
+type fileSystem struct {
+	logger *log.Logger
+	params config.Params
+}
+
+type FileSystem interface {
+	Run(context.Context, <-chan *kafka.ReadModel, *sync.WaitGroup)
+}
+
+type ErrorType string
+
+func (f *fileSystem) findDirectories(workflowId string, relativeWorkspacePath string) ([]string, error) {
 	var dirsToDelete []string
 	time.Sleep(8 * time.Second)
-	filepath.WalkDir(relativeWorkspacePath,
+	err := filepath.WalkDir(
+		relativeWorkspacePath,
 		func(path string, d fs.DirEntry, err error) error {
 			if err != nil {
 				return nil
 			}
 			if d.IsDir() && d.Name() == workflowId {
-				logDataDog.Info("Path found: ", path, "for workflow ID: ", workflowId)
+				f.logger.Info("Path found: ", path, "for workflow ID: ", workflowId)
 				dirsToDelete = append(dirsToDelete, path)
 			}
 			return nil
-		})
-	return dirsToDelete
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+	return dirsToDelete, nil
 }
 
-func deleteDirectory(directoryPath string, workflowId string) ErrorType {
+func (f *fileSystem) deleteDirectory(directoryPath string, workflowId string) ErrorType {
 	if _, err := os.Stat(directoryPath); err != nil {
 		if os.IsNotExist(err) {
-			logDataDog.WithFields(log.Fields{
+			f.logger.WithFields(log.Fields{
 				"context":    "deleting-directory",
 				"workflowId": workflowId,
 			}).Info(fmt.Sprintf("INFO: Directory doesnt exists: ", directoryPath))
@@ -56,7 +70,7 @@ func deleteDirectory(directoryPath string, workflowId string) ErrorType {
 	errRemove := os.RemoveAll(directoryPath)
 	if errRemove != nil {
 		if os.IsPermission(errRemove) {
-			logDataDog.WithFields(log.Fields{
+			f.logger.WithFields(log.Fields{
 				"context":    "deleting-directory",
 				"workflowId": workflowId,
 			}).Error(fmt.Sprintf("ERROR: Service doesnt have rights to access: ", directoryPath))
@@ -67,10 +81,10 @@ func deleteDirectory(directoryPath string, workflowId string) ErrorType {
 	return NoError
 }
 
-func deleteDirectories(dirs []string, workflowId string) ErrorType {
+func (f *fileSystem) deleteDirectories(dirs []string, workflowId string) ErrorType {
 	var allDeleted []ErrorType
 	for _, directoryPath := range dirs {
-		allDeleted = append(allDeleted, deleteDirectory(directoryPath, workflowId))
+		allDeleted = append(allDeleted, f.deleteDirectory(directoryPath, workflowId))
 	}
 	for _, x := range allDeleted {
 		if x != NoError {
@@ -81,15 +95,15 @@ func deleteDirectories(dirs []string, workflowId string) ErrorType {
 }
 
 // TODO add workflowEvent parent model and separate logic of parsing message and cleaning directory
-func resolveMessage(message string, relativePath string) ErrorType {
+func (f *fileSystem) resolveMessage(message string, relativePath string) ErrorType {
 	// json decode
 	var msgValue map[string]interface{}
 	if err := json.Unmarshal([]byte(message), &msgValue); err != nil {
-		logDataDog.Error("ERROR: Something went wrong in parsing received message :", msgValue)
+		f.logger.Error("ERROR: Something went wrong in parsing received message :", msgValue)
 		return InvalidFormat
 	}
 	if _, statusExists := msgValue["status"]; !statusExists {
-		logDataDog.Error("ERROR: received message has incorrect format: ", message)
+		f.logger.Error("ERROR: received message has incorrect format: ", message)
 		return InvalidFormat
 	}
 	switch status := msgValue["status"].(string); status {
@@ -97,76 +111,85 @@ func resolveMessage(message string, relativePath string) ErrorType {
 		success := events.WorkflowSuccess{}
 		err := json.Unmarshal([]byte(message), &success)
 		if err != nil {
-			logDataDog.Error("ERROR: while formating received message for success model: ", err)
+			f.logger.Error("ERROR: while formating received message for success model: ", err)
 			return InvalidFormat
 		}
 		success.DataDogLog("Received SUCCESS workflow event")
-		directoriesToDelete := findDirectories(success.WorkflowId, relativePath)
+		directoriesToDelete, err := f.findDirectories(success.WorkflowId, relativePath)
+		if err != nil {
+			f.logger.WithError(err).Debug("Error on walk")
+		}
 		if len(directoriesToDelete) < 1 {
 			success.DataDogLog(fmt.Sprintf("INFO: no directory has been found for workflowId: ", success.WorkflowId))
 			return NotFound
 		}
-		errType := deleteDirectories(directoriesToDelete, success.WorkflowId)
+		errType := f.deleteDirectories(directoriesToDelete, success.WorkflowId)
 		return errType
 	case "failure":
 		failure := events.WorkflowFailure{}
 		err := json.Unmarshal([]byte(message), &failure)
 		if err != nil {
-			logDataDog.Error("ERROR: while formating received message for failure model: ", err)
+			f.logger.Error("ERROR: while formating received message for failure model: ", err)
 			return InvalidFormat
 		}
 		failure.DataDogLog("Received FAILURE workflow event")
-		directoriesToDelete := findDirectories(failure.WorkflowId, relativePath)
+		directoriesToDelete, err := f.findDirectories(failure.WorkflowId, relativePath)
+		if err != nil {
+			f.logger.WithError(err).Debug("Error on walk")
+		}
 		if len(directoriesToDelete) < 1 {
 			failure.DataDogLog(fmt.Sprintf("INFO: no directory has been found for workflowId: ", failure.WorkflowId))
 			return NotFound
 		}
-		errType := deleteDirectories(directoriesToDelete, failure.WorkflowId)
+		errType := f.deleteDirectories(directoriesToDelete, failure.WorkflowId)
 		return errType
 	default:
-		logDataDog.Error("ERROR: Incorrect topic message status:", status)
+		f.logger.Error("ERROR: Incorrect topic message status:", status)
 		return InvalidFormat
 
 	}
 }
 
-func cleanWorkspace(data *kafka.ReadModel, params *config.Params) {
-	errType := resolveMessage(string(data.Message.Value), params.RelativePath)
+func (f *fileSystem) cleanWorkspace(data *kafka.ReadModel, params config.Params) {
+	errType := f.resolveMessage(string(data.Message.Value), params.RelativePath)
 	switch errType {
 	case NoError:
 		data.Session.MarkMessage(data.Message, "")
-		logDataDog.Info("EVENT MARKED for message:", string(data.Message.Value))
+		f.logger.Info("EVENT MARKED for message:", string(data.Message.Value))
 	case NotFound:
 		if params.MarkOnNotFoundFlag == true {
 			data.Session.MarkMessage(data.Message, "")
-			logDataDog.Info("EVENT MARKED for NotFoundError because flag -n is true, message:", string(data.Message.Value))
+			f.logger.Info("EVENT MARKED for NotFoundError because flag -n is true, message:", string(data.Message.Value))
 		} else {
-			logDataDog.Info("WARNING: event UNMARKED for message:", string(data.Message.Value))
+			f.logger.Info("WARNING: event UNMARKED for message:", string(data.Message.Value))
 		}
 	case NoAccess, InvalidFormat, GeneralError:
 		if params.MarkOnErrorFlag == true {
 			data.Session.MarkMessage(data.Message, "")
-			logDataDog.Info("EVENT MARKED for Error because flag -e is true, message:", string(data.Message.Value))
+			f.logger.Info("EVENT MARKED for Error because flag -e is true, message:", string(data.Message.Value))
 		} else {
-			logDataDog.Info("WARNING: event UNMARKED for message:", string(data.Message.Value))
+			f.logger.Info("WARNING: event UNMARKED for message:", string(data.Message.Value))
 		}
 	}
 }
 
-func RunInstance(readChan chan *kafka.ReadModel, doneChan chan bool, finalDoneChan chan bool, params *config.Params, logg *log.Logger) {
-	done := false
-	consumersStooped := 0
-	logDataDog = logg
-	for !done {
+func (f *fileSystem) Run(ctx context.Context, readChan <-chan *kafka.ReadModel, wg *sync.WaitGroup) {
+	for {
 		select {
 		case data := <-readChan:
-			go cleanWorkspace(data, params)
-		case <-doneChan:
-			consumersStooped++
-			if consumersStooped >= params.ConsumersNumber {
-				done = true
-			}
+			// Why you are using a goroutine here as it's totally useless and can generate too many threads and
+			// a data race or/and race condition for a same directory. Let this be synchronous...
+			f.cleanWorkspace(data, f.params)
+		case <-ctx.Done():
+			wg.Done()
+			return
 		}
 	}
-	finalDoneChan <- true
+}
+
+func New(params config.Params, logger *log.Logger) FileSystem {
+	return &fileSystem{
+		logger: logger,
+		params: params,
+	}
 }
